6649aca5549c4ba47bbc23428d43cecee678425c
RenLongHui <846276786@qq.com>
2019-06-27 08:58:31 +0800

    no message

--- http/config.js
+++ http/config.js
@@ -1,4 +1,8 @@
-const baseUrl="http://t.yushu.im/v2/movie/"
+
+const baseUrl=" https://web-gateway.hivescm.com com/ms-web" //开发环境
+// const baseUrl ="https://web-gateway.beehivescm.com com/ms-web" //测试环境
+// const baseUrl ="https://pre pre-web-gateway.newbeescm.com com/ms-web" //预发布环境
+// const baseUrl ="https://web-gateway.newbeescm.com com/ms-web"// 正式环境
 
 module.exports={
     in_theaters:baseUrl+"in_theaters",
