808c2591bd7c83654126cdd89b5be377bf4700a9
ZPP <1024529230@qq.com>
2019-06-27 17:37:48 +0800

    前三个的跳转页面

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,73 +1,113 @@
 // pages/my/my.js
-const app=getApp()
+const app = getApp()
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-        statusBarHeight:"",
-        phone:"17636643721",
-        userInfo:{}
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        statusBarHeight: "",
+        phone: "17636643721",
+        userInfo: {},
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function() {
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-      
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-      this.setData({
-          statusBarHeight: app.globalData.statusBarHeight,
-          userInfo: app.globalData.userInfo,
-          phone:this.data.phone
-      })
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+        this.setData({
+            statusBarHeight: app.globalData.statusBarHeight,
+            userInfo: app.globalData.userInfo,
+            phone: this.data.phone
+        })
+    },
+    getUserInfo: function(e) {
 
-  },
+        wx.getSetting({
+            success(res) {
+                if (!res.authSetting['scope.record']) {
+                    wx.authorize({
+                        scope: 'scope.record',
+                        success() {
+                            // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+                            wx.startRecord()
+                        }
+                    })
+                }
+            }
+        })
+    },
+    // 跳到收藏页
+    collect(){
+        wx.navigateTo({
+            url: '../../list/collect/collect',
+        })
+    },
+    // 跳到常购清单页
+    regularlist(){
+        wx.navigateTo({
+            url: '../../list/regularlist/regularlist',
+        })
+    },
+    // 跳到收货地址页
+    address(){
+        wx.navigateTo({
+            url: '../../list/address/address',
+        })
+    },
+    phone_button: function() {
+        wx.makePhoneCall({
+            phoneNumber: "400-666-0246"
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  },
+    },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
 
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
 
-  },
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
 
-  }
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
 })
