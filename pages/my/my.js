cbce3c8875256cb5774ae27cf3bf14e83cd87088
ZPP <1024529230@qq.com>
2019-06-28 09:29:17 +0800

    ddd

--- pages/my/my.js
+++ pages/my/my.js
@@ -16,7 +16,32 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function() {
-
+        if (app.globalData.userInfo) {
+            this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+            })
+        } else if (this.data.canIUse) {
+            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+            // 所以此处加入 callback 以防止这种情况
+            app.userInfoReadyCallback = res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    hasUserInfo: true
+                })
+            }
+        } else {
+            // 在没有 open-type=getUserInfo 版本的兼容处理
+            wx.getUserInfo({
+                success: res => {
+                    app.globalData.userInfo = res.userInfo
+                    this.setData({
+                        userInfo: res.userInfo,
+                        hasUserInfo: true
+                    })
+                }
+            })
+        }
     },
 
     /**
@@ -36,20 +61,12 @@ Page({
             phone: this.data.phone
         })
     },
-    getUserInfo: function(e) {
-
-        wx.getSetting({
-            success(res) {
-                if (!res.authSetting['scope.record']) {
-                    wx.authorize({
-                        scope: 'scope.record',
-                        success() {
-                            // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
-                            wx.startRecord()
-                        }
-                    })
-                }
-            }
+    getUserInfo: function (e) {
+        console.log(e)
+        app.globalData.userInfo = e.detail.userInfo
+        this.setData({
+            userInfo: e.detail.userInfo,
+            hasUserInfo: true
         })
     },
     // 跳到收藏页
