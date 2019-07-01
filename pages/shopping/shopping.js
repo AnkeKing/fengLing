73f86b6d40b507ec924277180cffcb8627c1f928
hemin <3105724316@qq.com>
2019-07-01 10:42:45 +0800

    购物车 订单

--- pages/shopping/shopping.js
+++ pages/shopping/shopping.js
@@ -1,19 +1,98 @@
 // pages/shopping/shopping.js
+const app= getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
-  data: {
-
-  },
+    data: {
+        statusBarHeight: app.globalData.statusBarHeight,
+        delBtnWidth: 160,
+        isScroll: true,
+        windowHeight: 0,
+        data: [{ content: "1", right: 0 }, { content: "2", right: 0 }, { content: "3", right: 0 }, { content: "4", right: 0 }, { content: "5", right: 0 }, { content: "6", right: 0 }, { content: "7", right: 0 }, { content: "8", right: 0 }, { content: "9", right: 0 }, { content: "10", right: 0 }],
+    },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+    onLoad: function (options) {
+        var that = this;
+        wx.getSystemInfo({
+            success: function (res) {
+                that.setData({
+                    windowHeight: res.windowHeight
+                });
+            }
+        });
+        //   console.log(app.globalData.statusBarHeight)
+    },
 
-  },
+    acknowledgement(){
+        wx.navigateTo({
+            url: '../acknowledgement/acknowledgement',
+        })
+    },
+
+    drawStart: function (e) {
+        // console.log("drawStart");  
+        var touch = e.touches[0]
+
+        for (var index in this.data.data) {
+            var item = this.data.data[index]
+            item.right = 0
+        }
+        this.setData({
+            data: this.data.data,
+            startX: touch.clientX,
+        })
+
+    },
+    drawMove: function (e) {
+        var touch = e.touches[0]
+        var item = this.data.data[e.currentTarget.dataset.index]
+        var disX = this.data.startX - touch.clientX
+
+        if (disX >= 20) {
+            if (disX > this.data.delBtnWidth) {
+                disX = this.data.delBtnWidth
+            }
+            item.right = disX
+            this.setData({
+                isScroll: false,
+                data: this.data.data
+            })
+        } else {
+            item.right = 0
+            this.setData({
+                isScroll: true,
+                data: this.data.data
+            })
+        }
+    },
+    drawEnd: function (e) {
+        var item = this.data.data[e.currentTarget.dataset.index]
+        if (item.right >= this.data.delBtnWidth / 2) {
+            item.right = this.data.delBtnWidth
+            this.setData({
+                isScroll: true,
+                data: this.data.data,
+            })
+        } else {
+            item.right = 0
+            this.setData({
+                isScroll: true,
+                data: this.data.data,
+            })
+        }
+    },
+
+    delItem: function (e) {
+
+    },
+
+
+
 
   /**
    * 生命周期函数--监听页面初次渲染完成
