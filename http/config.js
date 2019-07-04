349ff4d5f511b1d3eb4b322b6afb18a06d300fe1
hemin <3105724316@qq.com>
2019-07-04 16:35:10 +0800

    no message

--- http/config.js
+++ http/config.js
@@ -2,14 +2,17 @@
 
 // const baseUrl="https://web-gateway.hivescm.com/ms-web" //开发环境
 // const baseUrl ="https://web-gateway.beehivescm.com/ms-web" //测试环境
- const baseUrl ="https://pre-web-gateway.newbeescm.com/ms-web" //预发布环境
-//  const baseUrl ="https://web-gateway.newbeescm.com/ms-web"// 正式环境
+//  const baseUrl ="https://pre-web-gateway.newbeescm.com/ms-web" //预发布环境
+ const baseUrl ="https://web-gateway.newbeescm.com/ms-web"// 正式环境
 
 
 module.exports={
     in_theaters:baseUrl+"in_theaters",
-  baseUrl,
- collect: baseUrl + "/shopInfoController/getGoodsListByConditions",//收藏接口
+    baseUrl,
+    order: baseUrl + "/orderShoppingCart/getShoppingCartGroup", //购物车接口
+    minus: baseUrl + "/orderShoppingCart/subtractShoppingCart", //购物车商品数量减
+    collect: baseUrl + "/shopInfoController/getGoodsListByConditions",//收藏接口
     regularlist: baseUrl + "/order/alwaysBuyList",//常购清单
-
+    delCartGoods: baseUrl + '/orderShoppingCart/deleteShoppingCart',//删除购物车商品
+    shoppingQuantity: baseUrl + '/orderShoppingCart/getShoppingCartQuantity'
 }
