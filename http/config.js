e98578c7c51edabbb5d59a4583259efb590a42a1
RenLongHui <846276786@qq.com>
2019-07-08 20:10:27 +0800

    no message

--- http/config.js
+++ http/config.js
@@ -1,11 +1,7 @@
-
-
 // const baseUrl="https://web-gateway.hivescm.com/ms-web" //开发环境
 // const baseUrl ="https://web-gateway.beehivescm.com/ms-web" //测试环境
   //const baseUrl ="https://pre-web-gateway.newbeescm.com/ms-web" //预发布环境
  const baseUrl ="https://web-gateway.newbeescm.com/ms-web"// 正式环境
-
-
 module.exports={
     in_theaters:baseUrl+"in_theaters",
   baseUrl,
